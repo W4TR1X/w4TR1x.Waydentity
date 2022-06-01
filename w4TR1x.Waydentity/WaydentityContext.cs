e00a57b7553fd3c5dfe92efa22803a56ba847014
zeynelok <zeynel_2406@hotmail.com>
2022-06-02 00:13:26 +0300

    db context updated

--- w4TR1x.Waydentity/WaydentityContext.cs
+++ w4TR1x.Waydentity/WaydentityContext.cs
@@ -1,24 +1,30 @@
-﻿namespace w4TR1x.Waydentity;
+﻿using w4TR1x.Waydentity.Configurations;
 
-public abstract class WaydentityContext<TUserId, TTenantId, TRoleId> : DbContext
+namespace w4TR1x.Waydentity;
+
+public abstract class WaydentityContext<TUserId, TTenantId, TRoleId,TClaimId> : DbContext
     where TUserId : struct, IComparable, IComparable<TUserId>, IConvertible, IEquatable<TUserId>
     where TTenantId : struct, IComparable, IComparable<TTenantId>, IConvertible, IEquatable<TTenantId>
     where TRoleId : struct, IComparable, IComparable<TRoleId>, IConvertible, IEquatable<TRoleId>
+     where TClaimId : struct, IComparable, IComparable<TClaimId>, IConvertible, IEquatable<TClaimId>
 {
     public TTenantId CurrentTenantId { get; set; }
 
-    public DbSet<User<TUserId>> Users { get; set; } = null!;
-    public DbSet<Tenant<TTenantId>> Tenants { get; set; } = null!;
-    public DbSet<Role<TRoleId>> Roles { get; set; } = null!;
+    public DbSet<User<TUserId, TTenantId, TRoleId, TClaimId>> Users { get; set; } = null!;
+    public DbSet<Tenant<TUserId, TTenantId, TRoleId, TClaimId>> Tenants { get; set; } = null!;
+    public DbSet<Role<TUserId, TTenantId, TRoleId, TClaimId>> Roles { get; set; } = null!;
+    public DbSet<Claim<TUserId, TTenantId, TRoleId, TClaimId>> Claims { get; set; } = null!;
+    public DbSet<RoleClaim<TUserId,TTenantId,TRoleId,TClaimId>> RoleClaims { get; set; } = null!;
+    public DbSet<UserClaim<TUserId,TTenantId, TRoleId, TClaimId>> UserClaims { get; set; } = null!;
+    public DbSet<UserRole<TUserId, TTenantId, TRoleId, TClaimId>> UserRoles { get; set; } = null!;
 
     protected WaydentityContext(DbContextOptions options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<User<TUserId>>(c => c.Configure());
-        modelBuilder.Entity<Tenant<TTenantId>>(c => c.Configure());
-        modelBuilder.Entity<Role<TRoleId>>(c => c.Configure());
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(WaydentityContext<TUserId, TTenantId, TRoleId, TClaimId>).Assembly);
     }
 }
